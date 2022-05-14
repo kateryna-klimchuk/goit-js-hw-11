8c2b152d3efb04d2a5d37e3f5278bb7aded6150b
kateryna klimchuk <ketyclima@gmail.com>
2022-05-14 14:39:03 +0300

    main files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './sass/main.scss';
+import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+// Notiflix.Notify.warning('Memento te hominem esse');
+
+// Notiflix.Notify.info('Cogito ergo sum');
